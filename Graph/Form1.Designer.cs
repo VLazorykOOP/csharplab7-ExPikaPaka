e1d4afcad7fe11f957f0f016096917e18378cfc8
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2024-02-24 14:00:08 +0200

    Added graphic

--- Graph/Form1.Designer.cs
+++ Graph/Form1.Designer.cs
@@ -24,12 +24,185 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea13 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend13 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Tan;
+            chartArea13.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea13);
+            legend13.Name = "Legend1";
+            this.chart1.Legends.Add(legend13);
+            this.chart1.Location = new System.Drawing.Point(0, -1);
+            this.chart1.Name = "chart1";
+            series13.ChartArea = "ChartArea1";
+            series13.Legend = "Legend1";
+            series13.Name = "Series1";
+            this.chart1.Series.Add(series13);
+            this.chart1.Size = new System.Drawing.Size(699, 468);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(590, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 17);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(590, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(590, 388);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Pause";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(590, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(32, 17);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Yellow;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(590, 62);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(32, 17);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(626, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Grid and text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(626, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Background";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.trackBar1.Location = new System.Drawing.Point(590, 299);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(96, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(618, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Speed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(698, 460);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Graph/Form1.cs
+++ Graph/Form1.cs
@@ -7,11 +7,117 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Graph {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+
+            // Colors
+            colorDialog1.Color = Color.DarkMagenta;
+            button1.BackColor = Color.DarkMagenta;
+            button4.BackColor = Color.Black;
+            button5.BackColor = Color.FromArgb(255, 150, 150, 150);
+
+            // Slider
+            trackBar1.Value = 50;
+
+            // Timer
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+
+            plot();
+        }
+
+        // Colors
+        Color lineColor = Color.DarkMagenta;
+        Color textAndGridColor = Color.Black;
+        Color backgroundColor = Color.FromArgb(255, 180, 180, 180);
+
+        // Color buttons
+        private void button1_Click(object sender, EventArgs e) {
+            colorDialog1.ShowDialog();
+            button1.BackColor = colorDialog1.Color;
+            lineColor = colorDialog1.Color; 
+        }
+        private void button4_Click(object sender, EventArgs e) {
+            colorDialog1.ShowDialog();
+            button4.BackColor = colorDialog1.Color;
+            textAndGridColor = colorDialog1.Color;
+        }
+        private void button5_Click(object sender, EventArgs e) {
+            colorDialog1.ShowDialog();
+            button5.BackColor = colorDialog1.Color;
+            backgroundColor = colorDialog1.Color;
+        }
+
+
+        // Graphic
+        private double xS = -2;
+        private double xE = 0;
+        private double step = 0.01;
+        
+        void plot() {
+            // Create new series
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Line;
+
+            series.Color = lineColor;
+            series.BorderWidth = 3;
+
+            // Add points to the series
+            double x = xS; // Reset x
+            while (x <= xE) {
+                double y = (1 - x * x) * (x - 2);
+                series.Points.AddXY(x, y);
+                x += 0.01; // Step size for x
+            }
+            //xS += 0.01;
+            xE += step;
+
+            // Clear previous series
+            chart1.Series.Clear();
+
+            // Grid colors
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = textAndGridColor;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = textAndGridColor;
+            chart1.ChartAreas[0].AxisX.LabelStyle.ForeColor = textAndGridColor;
+            chart1.ChartAreas[0].AxisY.LabelStyle.ForeColor = textAndGridColor;
+            chart1.ChartAreas[0].AxisX.LineColor = textAndGridColor;
+            chart1.ChartAreas[0].AxisY.LineColor = textAndGridColor;
+
+            chart1.ChartAreas[0].BackColor = backgroundColor;
+
+
+
+            // Add series to the chart
+            chart1.Series.Add(series);
+        }
+        private void timer1_Tick(object sender, EventArgs e) {
+            plot();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            xS = -2;
+            xE = 0;
+
+            plot();
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            if(timer1.Enabled) {
+                timer1.Stop();
+            } else { 
+                timer1.Start(); 
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e) {
+            float value = (float)trackBar1.Value / 100.0f;
+
+            step = 0.02 * value;
+
         }
     }
 }
